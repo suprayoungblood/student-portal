4e13215bc018dc439cbb5e883ccd802e2bc2e8cc
suprayoungblood <parissjyoungblood@gmail.com>
2023-09-29 17:39:53 -0400

    fixed bootstrap errors

--- public/index.php
+++ public/index.php
@@ -3,9 +3,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Start the session
-session_start();
-
 require_once '../app/bootstrap.php';
 
 // Init Core Library
